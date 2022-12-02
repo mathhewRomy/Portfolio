75fe8b7344d58b002dda25fa7193cc3def25ac3b
Mathew Romy <matt_ry@pop-os.localdomain>
2022-12-02 16:36:32 +0530

    first commit

---
+++ src/components/about/About.jsx
@@ -0,0 +1,40 @@
+import "./about.css";
+import Award from "../../img/award.png";
+import Pic from "../../img/pic.jpg"
+
+const About = () => {
+  return (
+    <div className="a">
+      <div className="a-left">
+        <div className="a-card bg"></div>
+        <div className="a-card">
+          <img
+            src={Pic}
+            alt=""
+            className="a-img"
+          />
+        </div>
+      </div>
+      <div className="a-right">
+        <h1 className="a-title">About Me</h1>
+        <p className="a-sub">
+        </p>
+        <p className="a-desc">
+          I am very enthusiastic about Technology. I love AI and Machine learning, and am interested in concepts like super AI and making accurate models. I am also in interested in AR app development, and daily life integration. I also take interest in Web dev. I seek to expand my knowledge and make cool projects.
+        </p>
+        <div className="a-award">
+          <img src={Award} alt="" className="a-award-img" />
+          <div className="a-award-texts">
+            <h4 className="a-award-title">Projects Done</h4>
+            <p className="a-award-desc">
+              Nemo enim ipsam voluptatem quia voluptas sit aspernatur autodit
+              and fugit.
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default About;
